b252ea00f34a349a687236e10b9ea6264a2ab24b
786Nurjaman <nurjamanshekh@gmail.com>
2023-10-07 20:32:23 +0530

    f1

---
+++ src/Components/Down/Webdown.js
@@ -0,0 +1,12 @@
+import weblogo from '../Down/pngwing.com.png'
+const Webdown =()=>{
+    return(
+        <div className="container">
+        <img src={weblogo} alt="Your Logo" className="logo"/>
+        <h1>Website Under Maintenance</h1>
+        <p>We apologize for the inconvenience, but our website is currently undergoing maintenance. Please check back later.</p>
+    </div>
+    )
+}
+
+export default Webdown
